744a943c75e309f14af29273a0b93d77460b3020
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-10-24 00:04:36 +0200

    feat(service) : CSS des cards

--- components/Service/ServiceCard.vue
+++ components/Service/ServiceCard.vue
@@ -5,12 +5,17 @@ service: Array
 </script>
 
 <template>
-    <div v-for="(item, index) in service" :key="index">
-      
+    <div class="card__grid">
+        <div v-for="(item, index) in service" :key="index">
         <ServiceCardItem :title="item.service_title" :text="item.service_text" :image="item.service_icon" />
+    </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-
+.card__grid{
+    display: grid;
+    gap: rem(20);
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+}
 </style>
